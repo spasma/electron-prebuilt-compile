3e416af7fcc7d9863c5b556192a6042b3a94bb80
Sebastiaan Pasma <sebastiaan@enkodo.nl>
2019-02-25 12:04:14 +0100

    reverted to just the latest release version of electron

--- update.js
+++ update.js
@@ -3,12 +3,8 @@ const editJsonFile = require("edit-json-file");
 
 (async () => {
   let latestElectronVersion = await latestVersion('electron');
-  var config = require('./../../package.json'); // Get electron version from root packages.json
-  myElectronVersion = config.devDependencies.version;
-  console.log("Compiling for "+myElectronVersion);
-
   let packageFile = editJsonFile(`${__dirname}/package.json`);
-  packageFile.set("version", myElectronVersion);
-  packageFile.set('dependencies.electron', myElectronVersion);
+  packageFile.set("version", latestElectronVersion);
+  packageFile.set('dependencies.electron', latestElectronVersion);
   packageFile.save();
 })();
